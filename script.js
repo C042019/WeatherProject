ede5c86d1403a5d0b88464f5fa40f0ffc2f2dbe6
C042019 <69688404+C042019@users.noreply.github.com>
2020-11-09 17:46:45 -0600

    Update script.js
    updated js

--- script.js
+++ script.js
@@ -1,51 +1,62 @@
-function currentdate(date) {
-  let hours = currentTime.getHours();
-  let minutes = currentTime.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let dayIndex = currentTime.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  return `${days[dayIndex]} ${hours}:${minutes}`;
+function formatDate(timestamp){
+let date = new Date(timestamp);
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
 }
-function displayWeather(response) {
-  document.querySelector("#cityInput").innerHTML = response.data.main;
-  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed
-  );
-   document.querySelector("#feeltemp").innerHTML = Math.round(response.data.main.feels_like);
-  console.log(response.data);
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days= ["Sunday",
+"Monday",
+"Tuesday",
+"Wednesday",
+"Thursday",
+"Friday",
+"Saturday"
+]
+let day = days[date.getDay()];
+return `${day} ${hours}: ${minutes}`;
+
 }
 
-function search(city) {
-  let apiKey = "25ca3c40221589981906f4acf77d8a85";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(displayWeather);
-}
 
-function handleSubmit(event) {
-  event.preventDefault();
-  let city = document.querySelector("#cityInput").value;
-  search(city);
-}
 
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
+
+
+
+function displayTemperture(response)
+{ console.log(response.data);
+let temperatureElement = document.querySelector("#temperature");
+let windElement = document.querySelector("#wind");
+let humidityElement = document.querySelector("#humidity");
+let feelTempElement = document.querySelector("#feeltemp");
 let cityElement = document.querySelector("#city");
-let searchBar = document.querySelector("#cityChange");
+let dateElement = document.querySelector("#date");
+temperatureElement.innerHTML =Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+feelTempElement = Math.round(response.data.main.feels_like);
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = response.data.wind.speed;
+dateElement.innerHTML - formatDate(response.data.dt * 1000)
+};
+function search(city){
+  let apiKey ="25ca3c40221589981906f4acf77d8a85"
+let apiUrl =`api.openweathermap.org/data/2.5/weather?q=Dallas&appid=${apiKey}&units=farenheit`;
+axios.get(apiUrl).then(displayTemperture);
 
-searchBar.addEventListener("submit", handleSubmit);
+apiUrl =`api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=farenheit`;
+axios.get(apiUrl).then(displayForecast);
+}
 
-dateElement.innerHTML = currentdate(currentTime);
 
-search("Lewisville");
+function handleSubmit(event){
+  event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+
+
+let form = document.querySelector("#search-form")
+form.addEventListener("submit", handleSubmit);
